6c664995232676d45320597cd75a4b8c4f424755
Claire Melville <cmelville@lcs.on.ca>
2022-02-23 10:25:39 -0500

    Added the function "loadNewJoke" that asynchronously loads the data from our website service / endpoint

--- DadJokes/Views/ContentView.swift
+++ DadJokes/Views/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     //MARK: Stored Properties
-    var currentJoke: DadJoke = DadJoke(id: "",
+   @State var currentJoke: DadJoke = DadJoke(id: "",
                                        joke: "Knock knock...",
                                        status: 0)
     
@@ -21,6 +21,7 @@ struct ContentView: View {
             Text(currentJoke.joke)
                 .multilineTextAlignment(.center)
                 .font(.title)
+                .minimumScaleFactor(0.5)
                 .padding(30)
                 .overlay(
                     RoundedRectangle(cornerRadius: 16)
@@ -33,10 +34,15 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .padding(.bottom)
             
-            Button(action:{
-                print("I've been pressed.")
-            },
-                   label: {
+            Button(action: {
+                
+                // The Task type allows us to run asynchronous code within a button, and have the user interface be undated when the data is ready.
+                // Since it is asynchronous, other tasks can run while we wait for the data to come back from the web server
+                Task {
+                    // Call the function that will load a new joke
+                    await loadNewJoke()
+                }
+            }, label: {
                 Text("Another one!")
             })
                 .buttonStyle(.bordered)
@@ -62,8 +68,60 @@ struct ContentView: View {
             Spacer()
                         
         }
+        // When the app opens, get a new joke from the web service
+        .task {
+            
+            // Load a joke from the endpoint. We are "calling"/"invoking" the function named "loadNewJoke"
+            // A term for this is the "call site" of a function
+            // This just means that we as the programmer, are aware that this function is asynchronous.
+            // Result might come right away, or take some time to compltete. ALSO: Any code below this call will run before the function call completes.
+            await loadNewJoke()
+            
+            print("I tried to load a new joke")
+            
+        }
         .navigationTitle("icanhazdadjoke?")
         .padding()
+    }
+    
+    //MARK: Functions
+    
+    // Define the function "loadNewJoke". Teaching our app to do a new thing
+    // Using the "async" keyword means that this function can potentially be run alongside other tasks the app needs to do
+    func loadNewJoke() async {
+        // Assemble the URL that points to the endpoint
+        let url = URL(string: "https://icanhazdadjoke.com/")!
+        
+        // Define the type of data we want from the endpoint
+        // Configure the request to the web site
+        var request = URLRequest(url: url)
+        // Ask for JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
+        
+        // Try to fetch a new joke
+        // It might not work, so we use a do-catch block
+        do {
+            
+            // Get the raw data from the endpoint
+            let (data, _) = try await urlSession.data(for: request)
+            
+            // Attempt to decode the raw data into a Swift structure
+            // Takes what is in "data" and tries to put it into "currentJoke"
+            //                                 DATA TYPE TO DECODE TO
+            //                                         |
+            //                                         V
+            currentJoke = try JSONDecoder().decode(DadJoke.self, from: data)
+            
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            // Print the contents of the "error" constant that the do-catch block
+            // populates
+            print(error)
+        }
     }
     
 }
